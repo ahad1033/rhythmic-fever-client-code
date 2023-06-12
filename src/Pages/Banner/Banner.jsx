3f67d27ab2d237bb8f0c1c53eeda3ad92272d04d
ahad1033 <a.ahad2232@gmail.com>
2023-06-12 15:13:57 +0600

    banner section added

---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,47 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+
+const Banner = () => {
+    return (
+        <Carousel className="my-2">
+            <div>
+                <img src="https://www.teahub.io/photos/full/311-3113938_hip-hop-girls-hd-desktop-wallpaper-hip-hop.jpg" />
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white  bg-black rounded-lg bg-opacity-80 px-2 py-2">
+                    <h2 className="text-5xl font-bold py-2 px-4">Step into Summer, Dance into Fun!</h2>
+                    <p className="mt-2">Experience the vibrant energy of summer as you embark on a thrilling journey of dance. At our summer camp, you will discover a world of rhythm, movement, and creativity. From hip-hop to ballet, jazz to contemporary, our diverse dance classes offer a rich tapestry of styles to explore.</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://wallpaper.dog/large/10736626.jpg" />
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white  bg-black rounded-lg bg-opacity-80 px-2 py-2">
+                    <h2 className="text-5xl font-bold py-2 px-4">Step into Summer, Dance into Fun!</h2>
+                    <p className="mt-2">Experience the vibrant energy of summer as you embark on a thrilling journey of dance. At our summer camp, you will discover a world of rhythm, movement, and creativity. From hip-hop to ballet, jazz to contemporary, our diverse dance classes offer a rich tapestry of styles to explore.</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://cutewallpaper.org/21/hip-hop-dance-wallpaper/Hip-Hop-Dance-Wallpapers-75-background-pictures-.jpg" />
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white  bg-black rounded-lg bg-opacity-80 px-2 py-2">
+                    <h2 className="text-5xl font-bold py-2 px-4">Step into Summer, Dance into Fun!</h2>
+                    <p className="mt-2">Experience the vibrant energy of summer as you embark on a thrilling journey of dance. At our summer camp, you will discover a world of rhythm, movement, and creativity. From hip-hop to ballet, jazz to contemporary, our diverse dance classes offer a rich tapestry of styles to explore.</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://wallpaperaccess.com/full/3662682.jpg" />
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white  bg-black rounded-lg bg-opacity-80 px-2 py-2">
+                    <h2 className="text-5xl font-bold py-2 px-4">Step into Summer, Dance into Fun!</h2>
+                    <p className="mt-2">Experience the vibrant energy of summer as you embark on a thrilling journey of dance. At our summer camp, you will discover a world of rhythm, movement, and creativity. From hip-hop to ballet, jazz to contemporary, our diverse dance classes offer a rich tapestry of styles to explore.</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://wallpaperaccess.com/full/7694762.jpg" />
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white  bg-black rounded-lg bg-opacity-80 px-2 py-2">
+                    <h2 className="text-5xl font-bold py-2 px-4">Step into Summer, Dance into Fun!</h2>
+                    <p className="mt-2">Experience the vibrant energy of summer as you embark on a thrilling journey of dance. At our summer camp, you will discover a world of rhythm, movement, and creativity. From hip-hop to ballet, jazz to contemporary, our diverse dance classes offer a rich tapestry of styles to explore.</p>
+                </div>
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
