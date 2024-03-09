db8213954ce450eaf626216040a9c52fdf49e2a8
ahad1033 <a.ahad2232@gmail.com>
2024-03-09 15:01:19 +0600

    made changes in styles and others ui issues

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,13 +3,13 @@ import ExtraSection from "../../ExtraSection/ExtraSection";
 import PopularClasses from "../../PopularClasses/PopularClasses";
 
 const Home = () => {
-    return (
-        <>
-            <Banner />
-            <PopularClasses />
-            <ExtraSection />
-        </>
-    );
+  return (
+    <>
+      <Banner />
+      <PopularClasses />
+      <ExtraSection />
+    </>
+  );
 };
 
 export default Home;
